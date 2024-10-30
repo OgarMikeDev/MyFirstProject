96aaca7b7899d2679e715601c31ab13a2f84ac9d
ogar_m <ogar.m.v@mail.ru>
2024-10-30 13:00:51 +0300

    ogar branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ogar_branch");
+        System.out.println("Введите число: ");
+        int value = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= value; i++) {
+            for (int j = value; j >= 1; j--) {
+                if (i * j == value) {
+                    System.out.println(i + " * " + j);
+                }
+            }
+        }
     }
 }
